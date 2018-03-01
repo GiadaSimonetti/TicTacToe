20b7a305dbfa272ba92541c85ef92719a97dc8b0
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-03-01 14:23:05 +0000

    Add turn

--- spec/turnSpec.js
+++ spec/turnSpec.js
@@ -1,11 +1,21 @@
-// describe("Feature test", function() {
-//   var turn;
-//
-//   beforeEach(function() {
-//     turn = new Turn();
-//   });
-//
-//   it("sets the first turn as X", function() {
-//     expect(turn.initialBalance).toEqual('X');
-//   });
-// });
+describe("Feature test", function() {
+  var turn;
+  var player;
+
+  beforeEach(function() {
+    turn = new Turn(player);
+    player = new Player();
+  });
+
+  it("sets the first move at 0", function() {
+    expect(turn.FIRST_MOVE).toEqual(0);
+  });
+
+  it("sets the last move at 9", function() {
+    expect(turn.LAST_MOVE).toEqual(9);
+  });
+
+  it("sets the current move at 0", function() {
+    expect(turn.currentMove).toEqual(0);
+  });
+});
---
+++ src/turn.js
@@ -0,0 +1,13 @@
+function Turn(player) {
+  this.FIRST_MOVE = 0;
+  this.LAST_MOVE = 9;
+  this.currentMove = this.FIRST_MOVE;
+}
+
+// Turn.prototype.swapPlayer = function () {
+//   if (true) {
+//
+//   } else {
+//
+//   }
+// };

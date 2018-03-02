941ed71869d3a8199a6208157fe7aa16e48f42a8
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-03-02 13:09:54 +0000

    Test grid class and print grid

--- spec/gridSpec.js
+++ spec/gridSpec.js
@@ -1,13 +1,33 @@
 describe("Feature test", function() {
-  var player;
-  var turn;
   var grid;
-  var game;
 
   beforeEach(function() {
-    player = new Player();
-    turn = new Turn(player);
     grid = new Grid();
-    game = new Game(turn);
+  });
+
+  it("should print out the grid", function() {
+    expect(grid.setGrid()).toEqual(
+      "\n" +
+        1 +
+        " | " +
+        2 +
+        " | " +
+        3 +
+        "\n" +
+        "---------\n" +
+        4 +
+        " | " +
+        5 +
+        " | " +
+        6 +
+        "\n" +
+        "---------\n" +
+        7 +
+        " | " +
+        8 +
+        " | " +
+        9 +
+        "\n"
+    );
   });
 });
--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,5 @@
-function Grid(turn) {
-  this.moves = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-  this.grid = "";
+function Grid() {
+  this.moves = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   this.showGrid();
 }
 
@@ -12,7 +11,6 @@ Grid.prototype.setGrid = function() {
     this.moves[1] +
     " | " +
     this.moves[2] +
-    " | " +
     "\n" +
     "---------\n" +
     this.moves[3] +
@@ -20,7 +18,6 @@ Grid.prototype.setGrid = function() {
     this.moves[4] +
     " | " +
     this.moves[5] +
-    " | " +
     "\n" +
     "---------\n" +
     this.moves[6] +
@@ -28,7 +25,6 @@ Grid.prototype.setGrid = function() {
     this.moves[7] +
     " | " +
     this.moves[8] +
-    " | " +
     "\n"
   );
 };
